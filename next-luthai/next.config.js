b36b698c59533b9c6bd89e7b379c404b6dcf0055
s446683009 <446683009@qq.com>
2021-12-15 13:34:14 +0800

    修改发布

--- next-luthai/next.config.js
+++ next-luthai/next.config.js
@@ -1,5 +1,8 @@
 const { i18n } = require('./next-i18next.config')
+const BASE_URL =process.env.NODE_ENV === "production"?'/next':'';
 
 module.exports = {
   i18n,
+  basePath: BASE_URL, //node
 }
+
