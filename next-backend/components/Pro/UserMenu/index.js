be9f5746a6ec3aa167691ff76a8fc69b31e0808f
s446683009 <446683009@qq.com>
2022-03-01 10:16:04 +0800

    添加个人菜单

--- next-backend/components/Pro/UserMenu/index.js
+++ next-backend/components/Pro/UserMenu/index.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import {Page,List,ListItem,ListItemIcon,ListItemText, Typography,IconButton,Avatar} from '@mui/material'
+import {Page,List,ListItem,ListItemText, Typography,Divider,Avatar} from '@mui/material'
 import PropTypes from 'prop-types'
 import ExpendMenu from '../../ExpendMenu/ClickExpendMenu'
 
@@ -9,26 +9,39 @@ import styled from '../index.module.css'
 function UserMenu({t}) {
 const parentRef=React.useRef(null);
 const iconComponent=  <Avatar src='https://avatars.githubusercontent.com/u/37138998?s=40&v=4'/>;
+const textSx={
+  my:0
+};
   return (
     
     <ExpendMenu id="user" sx={{p:0}} text={iconComponent} closeRef={parentRef}>
-      <>
+      <div className={styled.useWrap}>
       <div className={styled.titleWrap}>
         <div>
-         <Typography>{t("Notification")}</Typography>
-         <Typography>您有两个未读消息</Typography>
+         <Typography className={styled.title} component={"h5"}>SoloTravelling</Typography>
+         <Typography className={styled.subTitle}>446683009@qq.com</Typography>
         </div>
-        <div>
-          <IconButton>
-              <i className='iconfont icon-tongzhi1'></i>
-          </IconButton>  
-        </div>
-      </div>
     
-     <List>
-
-     </List>
-     </>
+      </div>
+     
+      <List>
+          <Divider light  sx={{my:0.8}} />
+        <ListItem className={styled.userItem} button>
+          <ListItemText sx={textSx} primary={"我的主页"}></ListItemText>
+        </ListItem>
+        <ListItem className={styled.userItem} button>
+          <ListItemText sx={textSx} primary={"个人资料"}></ListItemText>
+        </ListItem>
+        <ListItem className={styled.userItem} button> 
+          <ListItemText sx={textSx} primary={"设置"}></ListItemText>
+        </ListItem>
+        <Divider light  sx={{my:0.8}} />
+        <ListItem className={styled.userItem} button>
+          <ListItemText sx={textSx} primary={"登出"}></ListItemText>
+        </ListItem>
+      </List>
+      
+     </div>
   </ExpendMenu>
   )
 }
