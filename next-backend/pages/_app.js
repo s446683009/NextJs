21c51b13d4ee0271967c3b648e92451fc80727dc
s446683009 <446683009@qq.com>
2022-03-17 10:28:07 +0800

    修改css 注入顺序

--- next-backend/pages/_app.js
+++ next-backend/pages/_app.js
@@ -1,29 +1,35 @@
-
+import React from 'react';
 import { appWithTranslation } from 'next-i18next'
+import { StyledEngineProvider } from '@mui/material/styles';
 import '../public/app.css'
 import '../public/css/iconfont/iconfont.css'
 import getTheme from '../utils/getTheme'
 import { ThemeProvider } from '@mui/material/styles';
 import Head from 'next/head';
 
-const theme= getTheme();
 
-const MyApp = ({ Component, pageProps }) =>{
+const theme = getTheme();
 
-   
-    
-    return(
-      <>
+const MyApp = ({ Component, pageProps }) => {
+
+
+
+  return (
+    <React.Fragment>
       <Head>
-      <meta name="viewport" content="initial-scale=1, width=device-width" />
+        <meta name="viewport" content="initial-scale=1, width=device-width" />
       </Head>
+
       <ThemeProvider theme={theme}>
-            <Component {...pageProps} />
+        <StyledEngineProvider injectFirst>
+          <Component {...pageProps} />
+        </StyledEngineProvider>
+
       </ThemeProvider>
-      </>
-      
-    )
-    
+    </React.Fragment>
+
+  )
+
 }
 
 export default appWithTranslation(MyApp)
--- next-backend/pages/_document.js
+++ next-backend/pages/_document.js
@@ -11,8 +11,7 @@ class MyDocument extends Document {
         <Html>
             <Head>
               <link rel="stylesheet" type="text/css" href='/backend/reset.css'></link>
-              <link rel="stylesheet" type="text/css" charSet="UTF-8" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.min.css" /> 
-              <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick-theme.min.css" />  
+
             </Head>
             <body>
                 <Main />
